3fc551c67544b35db9d410d5d935d8fc41796a6b
BartekM101 <bartosz.maroszek.2000@gmail.com>
2022-11-26 06:36:10 -0500

    Repair wrong constructor name

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import re
 #from modules.selfdestruct import selfdestruct
 #from modules.re_entrancy import * 
 #from modules.unchecked_external_call import * 
-#from modules.wrong_constructor_name import * 
+from modules.wrong_constructor_name import * 
 ##from modules.stored_credentials import * 
 #from modules.insec_randomsource import * 
 #from modules.tx_origin import * 
@@ -30,7 +30,7 @@ def scan_contract(contract):
     #selfdestruct(contract)
     #reentrancy(contract)
     #unchecked_external_call(contract)
-    #wrong_constructor_name(contract)
+    wrong_constructor_name(contract)
     #stored_credentials(contract)
     #randomsource(contract)
     #tx_origin(contract)
--- modules/wrong_constructor_name.py
+++ modules/wrong_constructor_name.py
@@ -5,17 +5,27 @@ import ntpath # solves problem off full path or test catalog issue
 
 
 def wrong_constructor_name(contract):
-    contract_name_with_extension = ntpath.basename(contract) # ./test/Reentrancy.sol -> Reentrancy.sol
-    contract_name = contract_name_with_extension.split('.')[0] # Reentrancy.sol -> Reentrancy   
+    #contract_name_with_extension = ntpath.basename(contract) # ./test/Reentrancy.sol -> Reentrancy.sol
+    #contract_name = contract_name_with_extension.split('.')[0] # Reentrancy.sol -> Reentrancy   
+    #
+    r1 = re.compile('contract(.*?){', re.IGNORECASE)
+    parsed_contract_into_list2 = parse_contract(contract)
+    newlist2 = list(filter(r1.match, parsed_contract_into_list2))
+    contract_name_b = newlist2[0]
+    contract_name_b_2 = contract_name_b.split('{')
+    contract_name_b_3 = contract_name_b_2[0]
+    contract_name_b_4 = contract_name_b_3.split(' ')
+    contract_name = contract_name_b_4[1]
     #print(contract_name)
-    r = re.compile('^.*{}\(.*\)'.format(contract_name), re.IGNORECASE)
+    r = re.compile('^.*function {}\(.*\)'.format(contract_name), re.IGNORECASE)
     parsed_contract_into_list = parse_contract(contract)
     newlist = list(filter(r.match, parsed_contract_into_list))
+    #print(newlist)
 
     #===================================================================== 
     #check if list is empty == not proper constructor name
 
     if not newlist:
-        print(f"A contract probably uses bad constructor name - \"{contract_name}()\" function is misssing\n")
+        print(f"A contract probably uses bad constructor name - \"{contract_name}()\" function is misssing")
 
 
