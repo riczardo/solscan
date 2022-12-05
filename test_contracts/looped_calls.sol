13d07f1be86a20143915fa1165a481f39ab99781
riczardo <66421931+riczardo@users.noreply.github.com>
2022-12-05 10:23:28 -0500

    Create looped_calls.sol

---
+++ test_contracts/looped_calls.sol
@@ -0,0 +1,16 @@
+struct Payee {
+    address addr;
+    uint256 value;
+}
+
+Payee[] payees;
+uint256 nextPayeeIndex;
+
+function payOut() {
+    uint256 i = nextPayeeIndex;
+    while (i < payees.length && msg.gas > 200000) {
+      payees[i].addr.send(payees[i].value);
+      i++;
+    }
+    nextPayeeIndex = i;
+}
