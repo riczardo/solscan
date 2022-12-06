071943418ce0e64c1958eb08f1df87a9cb22d907
florkie <florekbartlomiej00@gmail.com>
2022-12-06 15:57:49 +0100

    deleted duplicate lines from stored creds

--- modules/stored_credentials.py
+++ modules/stored_credentials.py
@@ -33,6 +33,7 @@ def stored_credentials(contract):
             line_number = 1+parsed_contract_into_list.index(newlist[i]) #line number
             line_number_as_str = str(line_number) #line number to string
             newlist_to_print.append(line_number_as_str) #new list without []
-        newlist_printable = ', '.join(newlist_to_print) #new list without []
+        newlist_noduplics = list(dict.fromkeys(newlist_to_print))
+        newlist_printable = ', '.join(newlist_noduplics) #new list without []
             #Use printer
         printer_vuln(newlist_printable, vulnerability_name, vulnerability_description, vulnerability_recommendation, more_info)
